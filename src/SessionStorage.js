f215ecbde05c4454241af312085909a7fec1b2aa
Les Wardwell (lwardwel) <Les.Wardwell@quest.com>
2023-06-28 10:42:25 -0700

    TFS 421687: Update safeguard-js to use PKCE

--- src/SessionStorage.js
+++ src/SessionStorage.js
@@ -14,7 +14,12 @@ class SessionStorage {
     SessionStorageKeys = {
         ACCESSTOKEN: 'AccessToken',
         USERTOKEN:   'UserToken',
-        HOSTNAME:    'HostName'
+        HOSTNAME:    'HostName',
+        CODEVERIFIER: 'CodeVerifier',
+        RANDOMSTATE: 'RandomState',
+        CODE: 'Code',
+        STATE: 'State',
+        NEWLOGIN: 'NewLogin'
     }
 
     /**
@@ -23,6 +28,38 @@ class SessionStorage {
      */
     getAccessToken() {
         return sessionStorage.getItem(this.SessionStorageKeys.ACCESSTOKEN);
+    }
+
+    /**
+     * Gets the access pkce code from storage.
+     * @returns {string} The pkce code.
+     */
+    getCode() {
+        return sessionStorage.getItem(this.SessionStorageKeys.CODE);
+    }
+
+    /**
+     * Gets the access pkce code verifier from storage.
+     * @returns {string} The pkce verifier code.
+     */
+    getCodeVerifier() {
+        return sessionStorage.getItem(this.SessionStorageKeys.CODEVERIFIER);
+    }
+
+    /**
+     * Gets the access random state from storage.
+     * @returns {string} The random state.
+     */
+    getRandomState() {
+        return sessionStorage.getItem(this.SessionStorageKeys.RANDOMSTATE);
+    }
+
+    /**
+     * Gets the access pkce  state from storage.
+     * @returns {string} The pkce state.
+     */
+    getState() {
+        return sessionStorage.getItem(this.SessionStorageKeys.STATE);
     }
 
     /**
@@ -42,10 +79,46 @@ class SessionStorage {
     }
 
     /**
+     * Gets the new login flag from storage.
+     * @returns {string} The new login flag.
+     */
+    getNewLogin(){
+        return sessionStorage.getItem(this.SessionStorageKeys.NEWLOGIN);
+    }
+
+    /**
      * Writes the access token to storage.
      */
     setAccessToken(accessToken) {
         sessionStorage.setItem(this.SessionStorageKeys.ACCESSTOKEN, accessToken);
+    }
+
+    /**
+     * Writes the access pkce code to storage.
+     */
+    setCode(code) {
+        sessionStorage.setItem(this.SessionStorageKeys.CODE, code);
+    }
+
+    /**
+     * Writes the access pkce code verifier to storage.
+     */
+    setCodeVerifier(codeVerifier) {
+        sessionStorage.setItem(this.SessionStorageKeys.CODEVERIFIER, codeVerifier);
+    }
+
+    /**
+     * Writes the access random state to storage.
+     */
+    setRandomState(state) {
+        sessionStorage.setItem(this.SessionStorageKeys.RANDOMSTATE, state);
+    }
+
+    /**
+     * Writes the access pkce  state to storage.
+     */
+    setState(state) {
+        sessionStorage.setItem(this.SessionStorageKeys.STATE, state);
     }
 
     /**
@@ -63,27 +136,34 @@ class SessionStorage {
     }
 
     /**
+     * Writes the new login flag to storage.
+     */
+    setNewLogin(newLogin) {
+        sessionStorage.setItem(this.SessionStorageKeys.NEWLOGIN, newLogin);
+    }
+
+     /**
+      * Clears code and state from storage.
+      */
+     clearPKCEStorage() {
+        sessionStorage.removeItem(this.SessionStorageKeys.CODE);
+        sessionStorage.removeItem(this.SessionStorageKeys.STATE);
+        sessionStorage.removeItem(this.SessionStorageKeys.CODEVERIFIER);
+        sessionStorage.removeItem(this.SessionStorageKeys.RANDOMSTATE);
+    }
+
+    /**
      * Clears all values from storage.
      */
     clearStorage() {
-        let userToken = this.getUserToken();
-        let accessToken = this.getAccessToken();
-        let sessionHostName = this.getHostName();
-
-        if (userToken)
-        {
-            this.setUserToken('');
-        }
-
-        if (accessToken)
-        {
-            this.setAccessToken('');
-        }
-
-        if (sessionHostName)
-        {
-            this.setHostName('');
-        }
+        sessionStorage.removeItem(this.SessionStorageKeys.USERTOKEN);
+        sessionStorage.removeItem(this.SessionStorageKeys.ACCESSTOKEN);
+        sessionStorage.removeItem(this.SessionStorageKeys.HOSTNAME);
+        sessionStorage.removeItem(this.SessionStorageKeys.CODE);
+        sessionStorage.removeItem(this.SessionStorageKeys.CODEVERIFIER);
+        sessionStorage.removeItem(this.SessionStorageKeys.STATE);
+        sessionStorage.removeItem(this.SessionStorageKeys.RANDOMSTATE);
+        sessionStorage.removeItem(this.SessionStorageKeys.NEWLOGIN);
     }
 }
 
