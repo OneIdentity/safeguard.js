976659e8a861414f4d34ef77f721d0506c6d44c2
petrsnd <petrsnd@gmail.com>
2026-05-18 07:52:18 -0600

    Rework the pipeline, switch to Ubuntu build, add CODEOWNERS

---
+++ pipeline-templates/versionnumber.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)][string]$BuildId,
+    [Parameter(Mandatory=$true)][string]$TagName,
+    [Parameter(Mandatory=$true)][string]$IsTagBuild
+)
+
+$SemanticVersion = (node -p "require('./package.json').version").Trim()
+$IsTagBuildBool = $IsTagBuild -eq "True" -or $IsTagBuild -eq "true" -or $IsTagBuild -eq "1"
+
+if ($IsTagBuildBool) {
+    if ($TagName -notmatch '^v\d+\.\d+\.\d+') {
+        Write-Error "Tag '$TagName' does not match 'v<major>.<minor>.<patch>'"
+        exit 1
+    }
+    $PackageVersion = $TagName -replace '^v', ''
+} else {
+    $BuildNumber = $BuildId % 65534
+    $PackageVersion = "${SemanticVersion}-dev.${BuildNumber}"
+}
+
+npm pkg set version=$PackageVersion
+Write-Output "##vso[task.setvariable variable=PackageVersion;]$PackageVersion"
+
+if ($IsTagBuildBool) { $ReleaseTag = $TagName }
+else { $ReleaseTag = "dev/v${PackageVersion}" }
+Write-Output "##vso[task.setvariable variable=ReleaseTag;]$ReleaseTag"
+
+Write-Host "Version: $PackageVersion"
+Write-Host "ReleaseTag: $ReleaseTag"
